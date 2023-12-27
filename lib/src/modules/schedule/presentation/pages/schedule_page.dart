33ca22dc55eb3c3bf4ad63196caddd7b89f60b76
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-27 16:06:38 -0300

    criando dropdown button

--- lib/src/modules/schedule/presentation/pages/schedule_page.dart
+++ lib/src/modules/schedule/presentation/pages/schedule_page.dart
@@ -1,22 +1,28 @@
 import 'package:client_barber_shop/src/common_widgets/app_bar_widget.dart';
 import 'package:client_barber_shop/src/modules/schedule/domain/entities/barber_entity.dart';
+import 'package:client_barber_shop/src/modules/schedule/domain/entities/payment_methods_entity.dart';
+import 'package:client_barber_shop/src/modules/schedule/domain/entities/services_entity.dart';
 import 'package:client_barber_shop/src/modules/schedule/presentation/bloc/schedule_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SchedulePage extends StatefulWidget {
   final ScheduleBloc controller;
-  const SchedulePage({super.key, required this.controller});
+
+  const SchedulePage({Key? key, required this.controller}) : super(key: key);
 
   @override
   State<SchedulePage> createState() => _SchedulePageState();
 }
 
 class _SchedulePageState extends State<SchedulePage> {
+  String? selectedBarber;
+  String? selectedServices;
+  String? selectedPay;
+
   @override
   void initState() {
     super.initState();
-    // passa um evento geral do bloc aqui
     widget.controller.add(ScheduleEvent());
   }
 
@@ -24,39 +30,78 @@ class _SchedulePageState extends State<SchedulePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarWidget(title: 'Agendar'),
-      body: BlocBuilder(
-        bloc: widget.controller,
-        builder: (context, state){
-          if(state is ScheduleErrorState){
-            return Text(state.error.message.toString());
-          }
-          if(state is ScheduleLoadingState){
-            return const CircularProgressIndicator();
-          }
-          if(state is ScheduleSucessState){
-            final itemsBarbers = state.barber;
-            return Expanded(
-              child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocBuilder<ScheduleBloc, BlocState>(
+          bloc: widget.controller,
+          builder: (context, state) {
+            if (state is ScheduleErrorState) {
+              return Text(state.error.message.toString());
+            }
+            if (state is ScheduleLoadingState) {
+              return const CircularProgressIndicator();
+            }
+            if (state is ScheduleSucessState) {
+              final itemsBarbers = state.barber;
+              final itemsServices = state.services;
+              final itemsPay = state.pay;
+        
+              return Column(
                 children: [
-                  const Text('Barbeiro'),
+                  const Text('Barbeiro', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                   DropdownButtonFormField<String>(
+                    value: selectedBarber,
                     hint: const Text("Selecione um barbeiro"),
-                    onChanged: (String? value) {  },
-                    items: itemsBarbers.map((BarberEntity e){
+                    onChanged: (value) {
+                      setState(() {
+                        selectedBarber = value;
+                      });
+                    },
+                    items: itemsBarbers.map((BarberEntity e) {
                       return DropdownMenuItem<String>(
                         value: e.name,
                         child: Text(e.name!),
                       );
-                    } 
-                    ).toList(), 
-                  )
+                    }).toList(),
+                  ),
+                  const Text('Serviços', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  DropdownButtonFormField<String>(
+                    value: selectedServices,
+                    hint: const Text("Selecione um barbeiro"),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedServices = value;
+                      });
+                    },
+                    items: itemsServices.map((ServicesEntity e) {
+                      return DropdownMenuItem<String>(
+                        value: e.name,
+                        child: Text(e.name!),
+                      );
+                    }).toList(),
+                  ),
+                  const Text('Métodos de Pagamento', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  DropdownButtonFormField<String>(
+                    value: selectedPay,
+                    hint: const Text("Selecione um barbeiro"),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedPay = value;
+                      });
+                    },
+                    items: itemsPay.map((PaymentMethodsEntity e) {
+                      return DropdownMenuItem<String>(
+                        value: e.name,
+                        child: Text(e.name!),
+                      );
+                    }).toList(),
+                  ),
                 ],
-              )
-            );
-          }
-          return Container();
-
-        }
+              );
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
