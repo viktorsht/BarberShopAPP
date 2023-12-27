e898b553a400e288075d5e1d3046612903b19bdc
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-27 10:24:23 -0300

    criando schedule module

---
+++ lib/src/modules/schedule/schedule_module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class ScheduleModule extends Module{
+
+  @override
+  void binds(Injector i) {
+  }
+
+  @override
+  void routes(RouteManager r) {
+  }
+}
