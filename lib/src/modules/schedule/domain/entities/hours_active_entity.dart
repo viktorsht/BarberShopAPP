84ca65c454357804d0a0ade2dfe6dd8c295f1b01
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-28 17:32:49 -0300

    refatorando hours_active_entity

--- lib/src/modules/schedule/domain/entities/hours_active_entity.dart
+++ lib/src/modules/schedule/domain/entities/hours_active_entity.dart
@@ -1,12 +1,14 @@
 class HoursActiveEntity {
   int? id;
+  int? day;
   String? time;
   bool? status;
 
-  HoursActiveEntity({this.id, this.time, this.status});
+  HoursActiveEntity({this.id, this.day, this.time, this.status});
 
   HoursActiveEntity.fromJson(Map<String, dynamic> json) {
     id = json['id'];
+    day =json['day'];
     time = json['time'];
     status = json['status'];
   }
@@ -14,6 +16,7 @@ class HoursActiveEntity {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
+    data['day'] = day;
     data['time'] = time;
     data['status'] = status;
     return data;
