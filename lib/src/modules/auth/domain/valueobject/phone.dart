f95b6139c8c97d24a0a53ce81f842481badb9ac4
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-30 10:08:36 -0300

    refatorando phone.dart

--- lib/src/modules/auth/domain/valueobject/phone.dart
+++ lib/src/modules/auth/domain/valueobject/phone.dart
@@ -1,8 +1,10 @@
 import 'package:client_barber_shop/src/utils/value_object.dart';
 
+import '../../../../utils/phone_utils.dart';
+
 class Phone implements ValueObject {
   final String _value;
-  static final RegExp _emailRegex = RegExp(r'^\(\d{2}\) \d{4}-\d{4}$');
+  static final RegExp _phoneRegex = RegExp(r'^\(\d{2}\) \d{4}-\d{4}$');
 
   Phone(this._value);
 
@@ -10,7 +12,7 @@ class Phone implements ValueObject {
   String? validator() {
     if (_value.isEmpty) {
       return 'Adicione um número de telefone';
-    } else if (!_emailRegex.hasMatch(_value)) {
+    } else if (!_phoneRegex.hasMatch(formatPhoneNumber(_value))) {
       return 'Formato de telefone inválido';
     }
     return null;
